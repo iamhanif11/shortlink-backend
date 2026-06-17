f12fb21f22e47e76391f1c9d7d98f4570e47b227
M. Hanif Irfan <mhirfan173@gmail.com>
2026-06-17 20:57:17 +0700

    feat: add jwt and hash

---
+++ pkg/jwt.go
@@ -0,0 +1,61 @@
+package pkg
+
+import (
+	"errors"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Claims struct {
+	Id    int    `json:"id"`
+	Email string `json:"email"`
+	jwt.RegisteredClaims
+}
+
+func NewClaims(id int, email string) *Claims {
+	jwtDuration := 30 * time.Minute
+
+	return &Claims{
+		Id:    id,
+		Email: email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    os.Getenv("JWT_ISSUER"),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(jwtDuration)),
+		},
+	}
+}
+
+func (c *Claims) GenJWT() (string, error) {
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		return "", errors.New("Missing JWT Secret")
+	}
+	uToken := jwt.NewWithClaims(jwt.SigningMethodHS256, c)
+	return uToken.SignedString([]byte(jwtSecret))
+}
+
+func (c *Claims) VerifyJWT(tokenString string) (*Claims, error) {
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		return nil, errors.New("Missing JWT Secret")
+	}
+
+	jwtToken, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(t *jwt.Token) (any, error) {
+		return []byte(jwtSecret), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	claims, ok := jwtToken.Claims.(*Claims)
+	if !ok || !jwtToken.Valid {
+		return nil, errors.New("Invalid token claims")
+	}
+
+	if claims.Issuer != os.Getenv("JWT_ISSUER") {
+		return nil, jwt.ErrTokenInvalidIssuer
+	}
+
+	return claims, nil
+}
